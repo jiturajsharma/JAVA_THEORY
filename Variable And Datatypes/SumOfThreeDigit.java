12304ef09eb68bb031e9579031e711bad82a4fa5
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2024-07-15 17:26:37 +0530

    Introduction to java

---
+++ Variable And Datatypes/SumOfThreeDigit.java
@@ -0,0 +1,31 @@
+// /**
+//  * VariableAndDataTypes
+//  */
+// public class VariableAndDataTypes {
+//     public static void main(String[] args) {
+//         boolean Raj = false;
+//         System.out.println(Raj);
+//         }
+// }
+
+// byte num1 = 2;
+// int num2 = 32;
+// float num3 = 234f;
+// long num4 = 2432;
+// double num5 = 423;
+
+
+public class SumOfThreeDigit {
+    // Write a java program to three numbers
+public static void main(String[] args) {
+
+    int value1 = 43;
+    float value2 =4423;
+    double value3 = 564;
+    
+        // int sumOfThreeDigit = value1 + value2 + value3;
+        // System.out.println(sumOfThreeDigit);
+    }
+}
+
+
