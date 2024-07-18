d77ae64b4ab6525441ae8a9b540e6d836f5296d9
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2024-07-18 17:42:51 +0530

    condition theory part added

---
+++ ConditionsInJava/ConditionInJava.java
@@ -0,0 +1,15 @@
+package ConditionsInJava;
+
+public class ConditionInJava {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("The number is positive");
+        } else if (number == 0) {
+            System.out.println("The number is zero");
+        } else {
+            System.out.println("The number is negative");
+        }
+    }
+}
