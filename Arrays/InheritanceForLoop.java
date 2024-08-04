0b0cac89d8bbabebf9984380f79376fe1467a91f
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2024-08-04 15:06:17 +0530

    String in java

---
+++ Arrays/InheritanceForLoop.java
@@ -0,0 +1,21 @@
+
+class InheritanceForLoop {
+    public static void main(String[] args) {
+        
+        int[] nums = {4,5,6,7};
+        for (int sharma : nums) {
+            // System.out.println(nums.toString());
+            // System.out.println(nums.length);
+            // System.out.println(nums.clone());
+            // System.out.println(sharma);
+        }
+
+        String nameOfString [] = {"JITU", "SHARMA", "RITU", "VISHWAKARMA"};  
+            // System.out.println(nameOfString);
+
+            for (String NewName : nameOfString) {
+                System.out.println(NewName.length());
+            }
+    }
+    
+}
---
+++ String/FirstString.java
@@ -0,0 +1,69 @@
+// String is a refers as the collection of character which are inclose within double court's examples of String "JITU", "*#@&", "1234567"
+                // If we inclose in single clouse's then we all considers character's
+
+                
+    public class FirstString {
+                public static void main(String[] args) {
+                String nums = "JituKumar";
+                // String is a object method which is writen capital letter in java
+                System.out.println(nums);
+
+                // All data types written in small letters eg :- int, short, float double char boolen -> Primitive data types
+
+                // String :- is a written in first letter capital which is called String is a class and class is a bluprint is a object. So we called. String is a obj
+                                // Lang package in the presents' sting so that reason no mendatory t use the unport system.
+
+                //  -> In Object like string all types of object allocated memory in heap file 
+                // * Java.long   * String package will be created
+
+                // * In object like string all types of object alocated memory in heap file 
+
+
+/*
+ *                                                                              STRING
+ *              i.>   Immutable (Non-changable)                                        ii.>   Mutable String (changbale)
+ *                    *String                                                                                   *StringBuilder * StringBuffer
+                        
+ */
+
+
+                        String school = "Grand Valley Academy";
+                        System.out.println(school);
+                        school.concat("Jiturajsharma");
+        // Answer will be not updated value because String is a object and object is never changed
+                        System.out.println(school);
+
+                        StringBuffer ChangeValue = new StringBuffer("GRAND VALLEY");
+        // Answer will change because StringBuilder will change the value 
+                        System.out.println(ChangeValue);
+                        ChangeValue.append(school);
+                        System.out.println(ChangeValue);
+
+/*
+                                        //      JAVA IS CASE SENSESTIVE
+ *      IMMUTABLE STRING :- 
+ *                      String varName = "PW";
+ *                 String valueName  = new String("Pwskills")
+ *              char[] ch = {"PW", 'skills'}
+ *                      String varName = new String(ch);
+ *      String -> Object -> Heap
+ * 
+ * 
+ * 
+ *                      
+ */
+
+                        String s1 = "PW";     // it is takes String constant pool
+                        String s2 = new String("DATA");   // it is take heap memory
+        
+        //  -> String constant pool  is don't allow to make duplicate reference
+
+
+
+
+                        
+
+
+                }
+        
+}
