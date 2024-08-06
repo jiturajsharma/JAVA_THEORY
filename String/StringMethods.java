b875dc6165e503d2de05073153b12a0b17b8a7c4
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2024-08-06 17:01:37 +0530

    String methods added

---
+++ String/StringMethods.java
@@ -0,0 +1,21 @@
+
+
+public class StringMethods {
+                public static void main(String[] args) {
+                    String str = "JITU RAJ SHARMA";
+                    System.out.println(str);
+                    System.out.println(str.toLowerCase());
+                    System.out.println(str.toUpperCase());
+                    System.out.println(str.length());
+                    System.out.println(str.charAt(1));
+                    
+                    int length = str.length();
+                    System.out.println(length);
+                    System.out.println(str.substring(1, 3));
+                    System.out.println(str.indexOf("T"));
+                    System.out.println(str.indexOf("U"));
+                    System.out.println(str.lastIndexOf("A"));
+
+                }
+    
+}
