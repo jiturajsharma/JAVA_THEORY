57dda037589edc5a4eba028b6a44b138debe8a84
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2024-08-08 20:42:07 +0530

    added Priority Qeute And Trees methods

---
+++ CollectionAndMapAPI/PriorityQueueMethods.java
@@ -0,0 +1,25 @@
+package CollectionAndMapAPI;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueMethods {
+
+    public static void main(String[] args) {
+        PriorityQueue pq = new PriorityQueue();
+        pq.add(100);
+        pq.add(50);
+        pq.add(150);
+        pq.add(25);
+        pq.add(75);
+        pq.add(125);
+        pq.add(175);
+
+        
+        // it is store binary from of data
+        System.out.println(pq);
+        
+    }
+
+
+
+}
---
+++ CollectionAndMapAPI/TreeSetMethod.java
@@ -0,0 +1,19 @@
+package CollectionAndMapAPI;
+
+import java.util.TreeSet;
+
+public class TreeSetMethod {
+    public static void main(String[] args) {
+        TreeSet ts = new TreeSet();
+        ts.add(100);
+        ts.add(50);
+        ts.add(150);
+        ts.add(25);
+        ts.add(175);
+
+        // we can not order of prensesation 
+
+        // in output data is a shorted order
+        System.out.println(ts);
+    }
+}
